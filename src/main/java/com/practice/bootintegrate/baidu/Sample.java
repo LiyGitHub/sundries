2ab9ed22a91ba4e0c32013307407b697d32616b4
liy <761686949@qq.com>
2023-04-23 13:53:46 +0800

    提价代码

---
+++ src/main/java/com/practice/bootintegrate/baidu/Sample.java
@@ -0,0 +1,59 @@
+package com.practice.bootintegrate.baidu;
+
+import com.alibaba.fastjson.JSONObject;
+import okhttp3.*;
+
+import java.io.*;
+
+class Sample {
+    public static final String API_KEY = "1";
+    public static final String SECRET_KEY = "2";
+
+    static final OkHttpClient HTTP_CLIENT = new OkHttpClient().newBuilder().build();
+
+    public static void main(String []args) throws IOException{
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType, "{\"plan_id\":\"3\"}");
+        Request request = new Request.Builder()
+            .url("https://aip.baidubce.com/rpc/2.0/brain/solution/faceprint/verifyToken/generate?access_token=" + getAccessToken())
+            .method("POST", body)
+            .addHeader("Content-Type", "application/json")
+            .addHeader("Accept", "application/json")
+            .build();
+        Response response = HTTP_CLIENT.newCall(request).execute();
+        System.out.println(JSONObject.parseObject(response.body().string()).getJSONObject("result").getString("verify_token"));
+    }
+
+
+    /**
+     * 从用户的AK，SK生成鉴权签名（Access Token）
+     *
+     * @return 鉴权签名（Access Token）
+     * @throws IOException IO异常
+     */
+    static String getAccessToken() throws IOException {
+        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+        RequestBody body = RequestBody.create(mediaType, "grant_type=client_credentials&client_id=" + API_KEY
+                + "&client_secret=" + SECRET_KEY);
+        Request request = new Request.Builder()
+                .url("https://aip.baidubce.com/oauth/2.0/token")
+                .method("POST", body)
+                .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                .build();
+        Response response = HTTP_CLIENT.newCall(request).execute();
+        return JSONObject.parseObject(response.body().string()).getString("access_token");
+    }
+
+    static void getResult() throws IOException {
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType, "{\"verify_token\":\"4\"}");
+        Request request = new Request.Builder()
+                .url("https://aip.baidubce.com/rpc/2.0/brain/solution/faceprint/result/simple?access_token=" + getAccessToken())
+                .method("POST", body)
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Accept", "application/json")
+                .build();
+        Response response = HTTP_CLIENT.newCall(request).execute();
+        System.out.println(response.body().string());
+    }
+}
---
+++ src/main/java/com/practice/bootintegrate/baidu/SdkSample.java
@@ -0,0 +1,60 @@
+package com.practice.bootintegrate.baidu;
+
+import java.io.IOException;
+import java.util.*;
+
+import com.baidu.aip.face.AipFace;
+import org.json.JSONObject;
+import com.baidu.aip.ocr.AipOcr;
+public class SdkSample {
+    //设置APPID/AK/SK
+    public static final String APP_ID = "1";
+    public static final String API_KEY = "2";
+    public static final String SECRET_KEY = "3";
+
+    public static void main(String[] args) throws IOException {
+        // 初始化一个AipOcr
+        AipOcr client = new AipOcr(APP_ID, API_KEY, SECRET_KEY);
+        AipFace aipFace = new AipFace(APP_ID, API_KEY, SECRET_KEY);
+        // 可选：设置网络连接参数
+        client.setConnectionTimeoutInMillis(2000);
+        client.setSocketTimeoutInMillis(60000);
+        sample(client);
+        // 可选：设置代理服务器地址, http和socket二选一，或者均不设置
+//        client.setHttpProxy("proxy_host", proxy_port);  // 设置http代理
+//        client.setSocketProxy("proxy_host", proxy_port);  // 设置socket代理
+
+        // 可选：设置log4j日志输出格式，若不设置，则使用默认配置
+        // 也可以直接通过jvm启动参数设置此环境变量
+//        System.setProperty("aip.log4j.conf", "path/to/your/log4j.properties");
+
+        // 调用接口
+
+//        JSONObject res = client.basicGeneral(path, new HashMap<String, String>());
+//        System.out.println(res.toString(2));
+
+    }
+
+
+    public static void sample(AipOcr client) throws IOException {
+        // 传入可选参数调用接口
+        HashMap<String, String> options = new HashMap<String, String>();
+        options.put("detect_direction", "true");
+        options.put("detect_risk", "false");
+
+        String idCardSide = "back";
+
+        // 参数为本地图片路径
+        String image = "5";
+        JSONObject res = client.idcard(image, idCardSide, options);
+        System.out.println(res.toString(2));
+
+        // 参数为本地图片二进制数组
+//        byte[] file = Util.readFileByBytes(image);
+//        res = client.idcard(file, idCardSide, options);
+//        System.out.println(res.toString(2));
+
+    }
+
+
+}
